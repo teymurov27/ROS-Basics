610db510e8690d083615919137937312d2ca75c2
Samir Teymurov <56115472+teymurov27@users.noreply.github.com>
2021-03-26 21:38:10 +0300

    Update rpm_pub.py

--- getting rpm data from motor/rpm_pub.py
+++ getting rpm data from motor/rpm_pub.py
@@ -1,1 +1,24 @@
+import rospy
+from std_msgs.msg import Float32, String
+from numpy import random
+
+random.seed(None)
+rpm_num = random.randint(low = 50, high = 99, size = (1, 1))
+
+
+def rpm_pub():
+    rospy.init_node("rpm_pub_node")
+    pub = rospy.Publisher("rpm", Float32, queue_size=10)
+
+    rate = rospy.Rate(5)
+    while not rospy.is_shutdown():
+        pub.publish(rpm_num)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        rpm_pub()
+
+    except rospy.ROSInterruptException:
+        pass
 
