14935eca842908c6428b2bc861b32e3ac88875f5
Samir Teymurov <56115472+teymurov27@users.noreply.github.com>
2021-03-26 17:01:13 +0300

    Update subscriber.py

--- creating subscriber/subscriber.py
+++ creating subscriber/subscriber.py
@@ -1,1 +1,14 @@
+import rospy
+from std_msgs.msg import String
+
+def process_hello_world_msg(data):
+    print("Message Recieved " + str(data))
+
+def create_susbscriber():
+    rospy.init_node("hello_world_sub_node")
+    rospy.Subscriber("hello_world", String, process_hello_world_msg)
+
+if __name__ == '__main__':
+    create_susbscriber()
+    rospy.spin()
 
