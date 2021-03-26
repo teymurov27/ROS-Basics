f855d7d9025d5c0df29767c4b80d84515d04d5e9
Samir Teymurov <56115472+teymurov27@users.noreply.github.com>
2021-03-27 00:44:19 +0300

    Update speed_calc.py

--- getting rpm data from motor/speed_calc.py
+++ getting rpm data from motor/speed_calc.py
@@ -5,9 +5,10 @@
 import rospy
 from std_msgs.msg import Float32
 
-wheel_radius = 12.5 / 100 # Wheel Radius of robot in meters
+wheel_radius = rospy.get_param("/wheel_radius") # Wheel Radius of robot in meters
 
 def calc_speed(rpm, publisher):
+    wheel_radius = rospy.get_param("/wheel_radius")
     speed = rpm.data * 2 * 3.14159 / 60 * wheel_radius # speed in m/s
     publisher.publish(speed)
 
@@ -16,7 +17,6 @@ def create_susbscriber(pub):
 
 if __name__ == '__main__':
     rospy.init_node("speed_calc_sub_node")
-    pub = rospy.Publisher("speed", Float32, queue_size=10)
+    pub = pub = rospy.Publisher("speed", Float32, queue_size=10)
     create_susbscriber(pub)
     rospy.spin()
-
