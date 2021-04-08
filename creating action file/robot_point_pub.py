7d9d575e2d111c82a06673953d0f0757f99817cb
Samir Teymurov <56115472+teymurov27@users.noreply.github.com>
2021-04-08 12:26:56 +0300

    Update robot_point_pub.py

--- creating action file/robot_point_pub.py
+++ creating action file/robot_point_pub.py
@@ -1,1 +1,35 @@
+#! /usr/bin/env python3
+
+
+
+import rospy
+
+from geometry_msgs.msg import Point
+
+
+def robot_point_pub(user_coords):
+    pub = rospy.Publisher("robot/point", Point, queue_size=10)
+    print("Publishing...")
+
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        pub.publish(Point(x = user_coords[0], y = user_coords[1], z = user_coords[2]))
+        rate.sleep()
+
+
+
+if __name__ == "__main__":
+    try:
+        rospy.init_node("robot_point_pub_node")
+
+        user_x = input("What is the current robot x-coordinate: ")
+        user_y = input("What is the current robot y-coordinate: ")
+        user_z = input("What is the current robot z-coordinate: ")
+
+        user_coords = [float(user_x), float(user_y), float(user_z)]
+
+        robot_point_pub(user_coords)
+
+    except rospy.ROSInterruptException:
+        print("Exception Occured")
 
